2c906cfb9499f14c9d6e578d01322d0848e642a7
RIC217 <melericcardo9400@gmail.com>
2023-08-16 21:53:37 +0200

    Added test route

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+mod routes;
+
 use axum::{routing::get, Router};
+use routes::test::test;
+use sqlx::PgPool;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+async fn axum(
+    #[shuttle_shared_db::Postgres(
+        local_uri = "postgres://postgres:{secrets.PASSWORD}@localhost:5432/postgres"
+    )]
+    pool: PgPool,
+) -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().route("/", get(test)).with_state(pool);
 
     Ok(router.into())
 }
