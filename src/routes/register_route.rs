ab47c9b778b1abf3e9d96c2e65d937b2f6771c3d
RIC217 <melericcardo9400@gmail.com>
2023-08-21 08:43:33 +0200

    Added create_jwt function

--- src/routes/register_route.rs
+++ src/routes/register_route.rs
@@ -3,18 +3,24 @@ use axum::{
     response::{IntoResponse, Response},
     Json,
 };
+use hyper::Method;
 use hyper::StatusCode;
 use sha2::{Digest, Sha512};
 use shuttle_runtime::tracing::{info, warn};
+use shuttle_secrets::SecretStore;
 use time::OffsetDateTime;
-use hyper::Method;
 
-use crate::{structs::register_user::RegisterUser, utils::register::{generate_token, send_html_message}, AppState};
+use crate::{
+    structs::register_user::RegisterUser,
+    utils::register::{generate_token, send_html_message},
+    AppState,
+};
 
 pub async fn register_route(
     method: Method,
     State(app_state): State<AppState>,
     Json(register_user): Json<RegisterUser>,
+    #[shuttle_secrets::Secrets] secrets: SecretStore,
 ) -> Response {
     let mut hasher = Sha512::new();
     hasher.update(register_user.password);
@@ -26,16 +32,23 @@ pub async fn register_route(
         Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR, "Date invalide").into_response(),
     };
     let username = register_user.username.to_lowercase();
-    let email = format!("{}|{}", email_confirm_token, register_user.email.to_lowercase());
+    let email = format!(
+        "{}|{}",
+        email_confirm_token,
+        register_user.email.to_lowercase()
+    );
 
-    match match sqlx::query!("SELECT * FROM users WHERE email = $1", email).fetch_optional(&app_state.pool).await {
+    match match sqlx::query!("SELECT * FROM users WHERE email = $1", email)
+        .fetch_optional(&app_state.pool)
+        .await
+    {
         Ok(result) => result,
         Err(e) => {
             return (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response();
         }
     } {
         Some(_) => return (StatusCode::FORBIDDEN, "Adresse email déjà utilisée").into_response(),
-        None => ()
+        None => (),
     };
 
     match sqlx::query!("INSERT INTO users (username, email, password, birthdate, biography, is_male, token) VALUES ($1, $2, $3, $4, $5, $6, $7);", username, email, password, birthdate, register_user.biography, register_user.is_male, token).execute(&app_state.pool).await {
@@ -46,7 +59,14 @@ pub async fn register_route(
         }
     }
 
-    match sqlx::query!("INSERT INTO email_confirm (email, token) VALUES ($1, $2);", email, email_confirm_token).execute(&app_state.pool).await {
+    match sqlx::query!(
+        "INSERT INTO email_confirm (email, token) VALUES ($1, $2);",
+        email,
+        email_confirm_token
+    )
+    .execute(&app_state.pool)
+    .await
+    {
         Ok(_) => (),
         Err(e) => {
             info!("{e}");
@@ -54,7 +74,12 @@ pub async fn register_route(
         }
     }
 
-    match send_html_message(app_state.smtp_client, "Confirm email", &format!("<h1>Token : {}</h1>", email_confirm_token), register_user.email.parse().unwrap()) {
+    match send_html_message(
+        app_state.smtp_client,
+        "Confirm email",
+        &format!("<h1>Token : {}</h1>", email_confirm_token),
+        register_user.email.parse().unwrap(),
+    ) {
         Ok(_) => (),
         Err(e) => {
             warn!("{} `{}`", method, e.to_string());
--- src/utils/register.rs
+++ src/utils/register.rs
@@ -1,15 +1,42 @@
+use chrono::prelude::*;
+use chrono::Duration;
+use hyper::StatusCode;
+use jsonwebtoken::encode;
+use jsonwebtoken::EncodingKey;
+use jsonwebtoken::Header;
 use lettre::{
     message::{header::ContentType, Mailbox},
     Address, Message, SmtpTransport, Transport,
 };
 use rand::distributions::{Alphanumeric, DistString};
-use hyper::StatusCode;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use shuttle_runtime::tracing::log::error;
+use shuttle_secrets::SecretStore;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    exp: usize,
+    iat: usize,
+    sub: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserInfos {
+    id: i128,
+    permission: usize,
+}
 
 pub fn generate_token() -> String {
     Alphanumeric.sample_string(&mut rand::thread_rng(), 256)
 }
 
-pub fn send_html_message(smtp_client: SmtpTransport, subject: &str, msg: &str, to: Address) -> Result<(), String> {
+pub fn send_html_message(
+    smtp_client: SmtpTransport,
+    subject: &str,
+    msg: &str,
+    to: Address,
+) -> Result<(), String> {
     match smtp_client.send(
         &Message::builder()
             .from(Mailbox {
@@ -30,7 +57,26 @@ pub fn send_html_message(smtp_client: SmtpTransport, subject: &str, msg: &str, t
     }
 }
 
-pub fn create_jwt() -> Result<String, StatusCode> {
-    todo!("Create JWT");
-    Ok("".to_string())
+pub fn create_jwt(user_infos: UserInfos, secrets: SecretStore) -> Result<String, StatusCode> {
+    let mut now = Utc::now();
+    let iat = now.timestamp() as usize;
+    let exp_in = Duration::minutes(15);
+    now += exp_in;
+    let exp = now.timestamp() as usize;
+    let sub = json!(user_infos).to_string();
+    let claims = Claims { iat, exp, sub };
+    encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(
+            secrets
+                .get("ENCODING_KEY")
+                .expect("Veuillez renseigner le secret `ENCODING_KEY` dans Secrets.toml")
+                .as_bytes(),
+        ),
+    )
+    .map_err(|e| {
+        error!("Erreur lors de la création du token");
+        StatusCode::INTERNAL_SERVER_ERROR
+    })
 }
