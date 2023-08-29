751e3d42bcf0530bb9df243a01f9242db7690cc1
RIC217 <melericcardo9400@gmail.com>
2023-08-29 13:58:31 +0200

    Now shows the number of expired accounts deleted

--- src/utils/delete_not_activated_expired_accounts.rs
+++ src/utils/delete_not_activated_expired_accounts.rs
@@ -1,12 +1,22 @@
 use std::{thread, time::Duration};
 
-use shuttle_runtime::tracing::info;
+use shuttle_runtime::tracing::{info, error};
 use sqlx::PgPool;
+
+struct DeleteNotActivatedExpiredAccountsResult {
+    total: i64
+}
 
 pub async fn delete_not_activated_expired_accounts(pool: &PgPool) {
     loop {
-        sqlx::query!("DELETE FROM users WHERE email_verified = FALSE AND created_at + INTERVAL '10 minutes' < NOW()").execute(pool).await.unwrap();
-        info!("Deleted useless accounts");
+        let count = match sqlx::query_as!(DeleteNotActivatedExpiredAccountsResult, r#"WITH updated_rows AS (DELETE FROM users WHERE email_verified = FALSE AND created_at + INTERVAL '10 minutes' < NOW() RETURNING id) SELECT COUNT(id) AS "total!" FROM updated_rows"#).fetch_one(pool).await {
+            Ok(count) => count,
+            Err(e) => {
+                error!("{}", e);
+                return;
+            }
+        };
+        info!("Deleted {} useless account.s", count.total);
         thread::sleep(Duration::from_secs(60));
     }
 }
