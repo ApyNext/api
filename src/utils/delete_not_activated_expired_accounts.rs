9c63b31c3a8da5c94608a35c07b3e2134ff0a375
RIC217 <melericcardo9400@gmail.com>
2023-10-04 08:53:37 +0200

    Now delete useless accounts every hour instead of every minute

--- src/utils/delete_not_activated_expired_accounts.rs
+++ src/utils/delete_not_activated_expired_accounts.rs
@@ -11,6 +11,6 @@ pub async fn delete_not_activated_expired_accounts(pool: &PgPool) {
     loop {
         let count = sqlx::query_as!(DeleteNotActivatedExpiredAccountsResult, r#"WITH updated_rows AS (DELETE FROM users WHERE email_verified = FALSE AND created_at + INTERVAL '10 minutes' < NOW() RETURNING id) SELECT COUNT(id) AS "total!" FROM updated_rows"#).fetch_one(pool).await.unwrap();
         info!("Deleted {} useless account.s", count.total);
-        thread::sleep(Duration::from_secs(60));
+        thread::sleep(Duration::from_secs(3600));
     }
 }
